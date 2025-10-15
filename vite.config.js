20a14b59a7f7a4e0d62eb07f85d5fad6c9432c05
Leon Trofimenko <leontrofim228@gmail.com>
2025-10-15 10:13:26 +0700

    Исправлен base path для GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/', // Для собственного домена. Если используете github.io, замените на '/PhuketCharterTeam/'
+  base: '/PhuketCharterTeam/', // Для GitHub Pages
   server: {
     port: 3000
   }
