d61c74b1e94267b28b0516b9b53cff9be5e8bafe
Leon Trofimenko <leontrofim228@gmail.com>
2025-12-20 19:55:23 +0700

    Добавлена поддержка SPA роутинга для GitHub Pages и   исправлен H1

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { HashRouter } from 'react-router-dom';
+import { BrowserRouter } from 'react-router-dom';
 import App from './App';
 import './index.css';
 import { initAnalytics } from './utils/analytics';
@@ -12,8 +12,8 @@ window.addEventListener('DOMContentLoaded', () => {
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <HashRouter>
+    <BrowserRouter>
       <App />
-    </HashRouter>
+    </BrowserRouter>
   </React.StrictMode>
 );
